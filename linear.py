c8fe09640a42dc8bea9c4294608466be39294ce3
imanf-atima <imanfatima9512@gmail.com>
2026-05-06 12:34:55 +0400

    Update linear.py

--- linear.py
+++ linear.py
@@ -3,10 +3,16 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 df=pd.read_csv("data.csv")
-X=df[['HouseStudied']]
-Y=df['ExamScore']
+X=df.iloc[:, :-1].values
+Y=df.iloc[:, -1].values
 X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,random_state=42)
 model=LinearRegression()
 model.fit(X_train,Y_train)
 st.tilte("Exam Score Predictor")
 st.write("Enter hours studied to predict exam score")
+hours=st.number_input("Hours Studied:",min_value=0.0,step=0.1)
+if st.button("Predict Score"):
+  predicted_score=model.predict([hours])[0]
+  st.success(f"Predicted Score: {pridicted_score:.2f}")
+  st.write("### Sample Training Data')
+  st.dataframe(df)
